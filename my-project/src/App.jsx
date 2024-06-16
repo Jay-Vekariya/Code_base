f588a377ef21811387861a2e914dc4a49cc46492
Mr. Jay Patel <jayvekariya2003@gmail.com>
2024-06-16 23:31:22 +0530

    testing

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Homepage from "./Components/Homepage";
 import Features from "./Components/Features";
 import AboutUs from "./Components/AboutUs";
-
+// max-w-[2000px]
 const App = () => {
   return (
-    <div className="min-w-[1080px] max-w-[2000px]">
+    <div className="min-w-[1023px] min-h-[1000px]">
       <Homepage />
       <Features />
       <AboutUs />
--- my-project/src/Components/Homepage.jsx
+++ my-project/src/Components/Homepage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Homepage = () => {
   return (
-    <div className=" bg-sky-950 h-[700px] ">
+    <main className=" bg-sky-950 h-[700px]">
       {/* Website Logo  & Company Name*/}
       <div className="flex h-24 items-center">
         <img className="h-20" src="Images/WBLogo.jpg" />
@@ -10,25 +10,25 @@ const Homepage = () => {
       </div>
 
       {/*What Company does? */}
-      <div className="flex flex-row justify-between">
-        <div className="p-[60px] max-w-[750px] text-white font-serif flex flex-col gap-6 justify-center">
-          <p className=" text-[40px] text-wrap">
+      <div className="flex w-full justify-between">
+        <div className="p-[60px] max-w-[750px] md:max-w-[600px] text-white font-serif flex flex-col gap-6 justify-center">
+          <p className="text-[20px] font-extralight xl:text-[40px] lg:text-[40px] md:text-[30px] sm:text-[15px]  text-wrap xl:text-wrap lg:text-wrap md:text-wrap sm:text-wrap">
             Experience the future of healthcare with our innovative app
           </p>
-          <p className="text-[25px]">
+          <p className="text-[23px]  xl:text-[35px] lg:text-[35px] md:text-[25px] sm:text-[30px]  text-wrap xl:text-wrap lg:text-wrap md:text-wrap sm:text-wrap">
             Empowering you to take control of your well-being like never before.
             Download now and embark on a journey to a healthier, happier you.
           </p>
         </div>
 
-        <div className="h-[600px] felx flex-col justify-center lg:flex lg:justify-center lg:flex-col md:flex md:justify-center md:flex-col sm:flex sm:justify-center sm:flex-col">
+        <div className="h-[600px] felx flex-col justify-center lg:flex lg:justify-center lg:flex-col md:flex md:justify-center md:flex-col sm:flex sm:flex-col place-content-center">
           <img
-            className="object-fill xl:h-[500px] justify-end"
+            className="object-contain h-[700px] xl:h-[500px] lg:h-[450px] lg:object-contain md:h-[400px] md:object-contain sm:h-[380px] sm:object-contain justify-end"
             src="Images/WB_Main_Image.jpg"
           />
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
